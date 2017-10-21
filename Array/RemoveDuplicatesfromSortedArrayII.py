a2036f255bd329b6ff813f7e0705c7eddec39f5f
gengjiawen <technicalcute@gmail.com>
2017-10-21 19:45:55 +0800

    add two array problems

---
+++ Array/RemoveDuplicatesfromSortedArrayII.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii
+# 
+# Follow up for "Remove Duplicates":
+# What if duplicates are allowed at most _twice_?
+# For example,
+# Given sorted array _nums_ = `[1,1,1,2,2,3]`,
+# Your function should return length = `5`, with the first five elements of _nums_ being `1`, `1`, `2`, `2` and `3`. It doesn't matter what you leave beyond the new length.
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
---
+++ Array/RemoveElement.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/remove-element
+# 
+# Given an array and a value, remove all instances of that value in place and return the new length.
+# Do not allocate extra space for another array, you must do this in place with constant memory.
+# The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+# **Example:**
+# Given input array _nums_ = `[3,2,2,3]`, _val_ = `3`
+# Your function should return length = 2, with the first two elements of _nums_ being 2.
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        
