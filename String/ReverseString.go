b1d9888d2f1b32a55e2a9ce1fd48f9b61e0fa0b6
gengjiawen <technicalcute@gmail.com>
2018-03-03 18:15:40 +0800

    add reverse string go version

---
+++ String/ReverseString.go
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/reverse-string/description/
+// Write a function that takes a string as input and returns the string reversed.
+
+// Example:
+// Given s = "hello", return "olleh". 
+func reverseString(s string) string {
+	var c = strings.Split(s, "")
+
+	for i, j := 0, len(c)-1; i < j; i, j = i+1, j-1 {
+		c[i], c[j] = c[j], c[i]
+	}
+
+	return strings.Join(c, "")
+}
