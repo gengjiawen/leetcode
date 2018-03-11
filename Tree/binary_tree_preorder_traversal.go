335d1a7f2151bc66198816af7214265369789085
gengjiawen <technicalcute@gmail.com>
2018-03-11 21:15:17 +0800

    add binary-tree-preorder-traversal go version

---
+++ Tree/binary_tree_preorder_traversal.go
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/binary-tree-preorder-traversal
+// 
+// Given a binary tree, return the _preorder_ traversal of its nodes' values.
+// 
+// For example:
+// Given binary tree `[1,null,2,3]`,
+// 
+// ```
+//    1
+//     \
+//      2
+//     /
+//    3
+// ```
+// 
+// return `[1,2,3]`.
+// 
+// **Note:** Recursive solution is trivial, could you do it iteratively?
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+    res := []int{}
+    if (root != nil){
+    	res = append(res, root.Val)
+    	if root.Left != nil {
+			res = append(res, preorderTraversal(root.Left)...)
+		}
+		if root.Right != nil {
+			res = append(res, preorderTraversal(root.Right)...)
+		}
+    }
+
+    return res
+}
