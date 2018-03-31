eb98fec521d8b6265a6709a1a8186e61481c5f8e
gengjiawen <technicalcute@gmail.com>
2018-03-31 23:20:49 +0800

    add plus one go version

---
+++ Array/plus_one.go
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/plus-one
+// 
+// Given a non-negative integer represented as a **non-empty** array of digits, plus one to the integer.
+// 
+// You may assume the integer do not contain any leading zero, except the number 0 itself.
+// 
+// The digits are stored such that the most significant digit is at the head of the list.
+
+package main
+func plusOne(digits []int) []int {
+    for i := len(digits) - 1; i >= 0; i-- {
+        if digits[i] == 9 {
+            digits[i] = 0
+        } else {
+            digits[i] = digits[i] + 1
+            return digits
+        }
+    }
+
+    if digits[0] == 0 {
+        return append([]int{1}, digits...)
+    }
+
+    return digits
+}
