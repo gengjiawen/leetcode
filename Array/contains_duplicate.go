5501bdbda9ed17c9315ab2cc2c6719b31f3b0b71
gengjiawen <technicalcute@gmail.com>
2018-04-04 22:30:37 +0800

    add contains-duplicate go version

---
+++ Array/contains_duplicate.go
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/contains-duplicate
+//
+// Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+package main
+
+func containsDuplicate(nums []int) bool {
+	m := map[int]bool{}
+
+	for i := range nums {
+		if m[nums[i]] {
+			return true
+		}
+
+		m[nums[i]] = true
+	}
+
+	return false
+}
