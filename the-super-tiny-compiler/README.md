25575dc402863da53401e40790b8096f964262e4
gengjiawen <technicalcute@gmail.com>
2018-05-30 19:25:15 +0800

    add tiny compiler

---
+++ the-super-tiny-compiler/README.md
@@ -0,0 +1,7 @@
+The super tiny compiler typescript version
+------
+[![tested with jest](https://img.shields.io/badge/tested_with-jest-99424f.svg)](https://github.com/facebook/jest)
+
+The [original][1] is written in js, this is a port using typescript. Have fun :)
+
+[1]: https://github.com/thejameskyle/the-super-tiny-compiler
---
+++ the-super-tiny-compiler/the-super-tiny-compiler.test.ts
@@ -0,0 +1,15 @@
+import { codeGenerator, parser, tokenizer, transformer } from "./the-super-tiny-compiler";
+
+const input  = '(add 2 (subtract 4 2))';
+const output = 'add(2, subtract(4, 2));';
+
+it('tiny compiler', () => {
+    const tokens = tokenizer(input)
+    expect(tokens).toMatchSnapshot()
+    const ast = parser(tokens)
+    expect(ast).toMatchSnapshot()
+    let newAst = transformer(ast)
+    expect(newAst).toMatchSnapshot()
+    let output = codeGenerator(newAst)
+    expect(output).toMatch(output)
+})
