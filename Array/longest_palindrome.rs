9e686d3f502f629430055ca0eaf6154960df7cd0
gengjiawen <technicalcute@gmail.com>
2022-05-04 17:25:34 +0800

    fix: timeout

--- Array/longest_palindrome.rs
+++ Array/longest_palindrome.rs
@@ -11,32 +11,36 @@
 // Input: s = "cbbd"
 // Output: "bb"
 
-// elegant solution but timeout :(
+use std::ops::Range;
+
 pub fn longest_palindrome(s: String) -> String {
-    if s.len() <=1 {
+    if s.len() <= 1 {
         return s
     }
-    let mut longest_palindrome = String::new();
+
+    fn palindrome(s: &str, left: usize, right: usize) -> Range<usize> {
+        let mut l = left as i32;
+        let mut r = right;
+        // don't use s.chars().nth, it will timeout
+        // https://www.reddit.com/r/rust/comments/tbsffu/why_charsnthindexunwrap_is_so_slow/
+        let str_vec: Vec<char> = s.chars().collect();
+        while l >= 0 && r < s.len() && str_vec[l as usize] == str_vec[r] {
+            l -= 1;
+            r += 1;
+        }
+        return (l + 1) as usize..r;
+    }
+
+    let mut longest_palindrome = 0..0;
     for i in 0..s.len() {
         // s[i] centered longest palindrome (babab -> bab)
-        let s1 = palindrome(s.clone(), i, i);
+        let s1 = palindrome(&s, i, i);
         // s[i], s[i+1] centered longest palindrome (cbbd -> bb)0
-        let s2 = palindrome(s.clone(), i, i + 1);
-        longest_palindrome = [s1, s2, longest_palindrome.clone()].iter().max_by_key(|x| x.len()).unwrap().to_string();
+        let s2 = palindrome(&s, i, i + 1);
+        longest_palindrome = [s1, s2, longest_palindrome.clone()].iter().max_by_key(|x| x.end - x.start).unwrap().clone();
     }
 
-    return longest_palindrome;
-}
-
-
-pub fn palindrome(s: String, left: usize, right: usize) -> String {
-    let mut l = left as i32;
-    let mut r = right; // sorry, I am rust
-    while l >= 0 && r < s.len() && s.chars().nth(l as usize) == s.chars().nth(r) {
-        l -= 1;
-        r += 1;
-    }
-    return s[(l + 1) as usize..r].to_string();
+    return s[longest_palindrome.start..longest_palindrome.end].to_string();
 }
 
 
