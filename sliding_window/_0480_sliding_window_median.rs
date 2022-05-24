dd2f150e955c10dfba1bc5b5395336d4d7cab97b
gengjiawen <technicalcute@gmail.com>
2022-05-24 23:31:39 +0800

    fix: refactor rust solution arch

---
+++ sliding_window/_0480_sliding_window_median.rs
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/sliding-window-median
+//
+// The **median** is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. So the median is the mean of the two middle values.
+//
+// *   For examples, if `arr = [2,<u>3</u>,4]`, the median is `3`.
+// *   For examples, if `arr = [1,<u>2,3</u>,4]`, the median is `(2 + 3) / 2 = 2.5`.
+//
+// You are given an integer array `nums` and an integer `k`. There is a sliding window of size `k` which is moving from the very left of the array to the very right. You can only see the `k` numbers in the window. Each time the sliding window moves right by one position.
+//
+// Return _the median array for each window in the original array_. Answers within `10<sup>-5</sup>` of the actual value will be accepted.
+//
+// **Example 1:**
+//
+// ```
+// **Input:** nums = [1,3,-1,-3,5,3,6,7], k = 3
+// **Output:** [1.00000,-1.00000,-1.00000,3.00000,5.00000,6.00000]
+// **Explanation:**
+// Window position                Median
+// ---------------                -----
+// [**1  3  -1**] -3  5  3  6  7        1
+//  1 [**3  -1  -3**] 5  3  6  7       -1
+//  1  3 [**-1  -3  5**] 3  6  7       -1
+//  1  3  -1 [**-3  5  3**] 6  7        3
+//  1  3  -1  -3 [**5  3  6**] 7        5
+//  1  3  -1  -3  5 [**3  6  7**]       6
+// ```
+//
+// **Example 2:**
+//
+// ```
+// **Input:** nums = [1,2,3,4,2,3,1,4,2], k = 3
+// **Output:** [2.00000,3.00000,3.00000,3.00000,2.00000,3.00000,2.00000]
+// ```
+//
+// **Constraints:**
+//
+// *   `1 <= k <= nums.length <= 10<sup>5</sup>`
+// *   `-2<sup>31</sup> <= nums[i] <= 2<sup>31</sup> - 1`
+
+pub fn median_sliding_window(nums: Vec<i32>, k: i32) -> Vec<f64> {
+
+}
---
+++ sliding_window/lib.rs
@@ -0,0 +1,17 @@
+#![allow(dead_code)]
+
+mod _0003_length_of_longest_substring;
+mod _0005_longest_palindrome;
+mod _0159_longest_substring_with_at_most_two_distinc_characters;
+mod _0209_minimum_size_subarray_sum;
+mod _0217_contains_duplicate;
+mod _0567_permutation_in_string;
+mod _0643_maximum_average_subarray_i;
+mod _0438_find_all_anagrams_in_a_string;
+mod _0487_max_consecutive_ones_ii;
+mod _1004_max_consecutive_ones_iii;
+mod _1695_maximum_erasure_value;
+mod _1208_get_equal_substrings_within_budget;
+mod _1423_maximum_points_you_can_obtain_from_cards;
+mod _1052_grumpy_bookstore_owner;
+mod _1151_minimum_swaps_to_group_all_1s_together;
