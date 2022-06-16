1a9c752034406eece5a48aaf2ad31dbc7ea37a20
Jiawen Geng <technicalcute@gmail.com>
2022-06-16 04:19:18 +0000

    feat: add two more

--- Array/_1051_height_checker.rs
+++ Array/_1051_height_checker.rs
@@ -1,24 +1,24 @@
 // https://leetcode.com/problems/height-checker
-// 
+//
 // A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in **non-decreasing order** by height. Let this ordering be represented by the integer array `expected` where `expected[i]` is the expected height of the `i<sup>th</sup>` student in line.
-// 
+//
 // You are given an integer array `heights` representing the **current order** that the students are standing in. Each `heights[i]` is the height of the `i<sup>th</sup>` student in line (**0-indexed**).
-// 
+//
 // Return _the **number of indices** where_ `heights[i] != expected[i]`.
-// 
+//
 // **Example 1:**
-// 
+//
 // ```
 // **Input:** heights = [1,1,4,2,1,3]
 // **Output:** 3
-// **Explanation:** 
+// **Explanation:**
 // heights:  [1,1,<u>4</u>,2,<u>1</u>,<u>3</u>]
 // expected: [1,1,<u>1</u>,2,<u>3</u>,<u>4</u>]
 // Indices 2, 4, and 5 do not match.
 // ```
-// 
+//
 // **Example 2:**
-// 
+//
 // ```
 // **Input:** heights = [5,1,2,3,4]
 // **Output:** 5
@@ -27,9 +27,9 @@
 // expected: [<u>1</u>,<u>2</u>,<u>3</u>,<u>4</u>,<u>5</u>]
 // All indices do not match.
 // ```
-// 
+//
 // **Example 3:**
-// 
+//
 // ```
 // **Input:** heights = [1,2,3,4,5]
 // **Output:** 0
@@ -38,16 +38,23 @@
 // expected: [1,2,3,4,5]
 // All indices match.
 // ```
-// 
+//
 // **Constraints:**
-// 
+//
 // *   `1 <= heights.length <= 100`
 // *   `1 <= heights[i] <= 100`
 
 pub fn height_checker(heights: Vec<i32>) -> i32 {
-
-    }
+    let mut sorted_height = heights.clone();
+    sorted_height.sort_unstable();
+    return heights
+        .iter()
+        .zip(&sorted_height)
+        .filter(|(a, b)| a != b)
+        .count() as i32;
+}
 
 #[test]
 pub fn t1() {
+    assert_eq!(height_checker(vec![1, 1, 4, 2, 1, 3]), 3);
 }
--- Array/_1299_replace_elements_with_greatest_element_on_right_side.rs
+++ Array/_1299_replace_elements_with_greatest_element_on_right_side.rs
@@ -1,15 +1,15 @@
 // https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side
-// 
+//
 // Given an array `arr`, replace every element in that array with the greatest element among the elements to its right, and replace the last element with `-1`.
-// 
+//
 // After doing so, return the array.
-// 
+//
 // **Example 1:**
-// 
+//
 // ```
 // **Input:** arr = [17,18,5,4,6,1]
 // **Output:** [18,6,6,6,1,-1]
-// **Explanation:** 
+// **Explanation:**
 // - index 0 --> the greatest element to the right of index 0 is index 1 (18).
 // - index 1 --> the greatest element to the right of index 1 is index 4 (6).
 // - index 2 --> the greatest element to the right of index 2 is index 4 (6).
@@ -17,24 +17,46 @@
 // - index 4 --> the greatest element to the right of index 4 is index 5 (1).
 // - index 5 --> there are no elements to the right of index 5, so we put -1.
 // ```
-// 
+//
 // **Example 2:**
-// 
+//
 // ```
 // **Input:** arr = [400]
 // **Output:** [-1]
 // **Explanation:** There are no elements to the right of index 0.
 // ```
-// 
+//
 // **Constraints:**
-// 
+//
 // *   `1 <= arr.length <= 10<sup>4</sup>`
 // *   `1 <= arr[i] <= 10<sup>5</sup>`
 
 pub fn replace_elements(arr: Vec<i32>) -> Vec<i32> {
-
-    }
+    let mut r = arr
+        .iter()
+        .rev()
+        .fold((vec![-1], -1), |mut acc, &x| {
+            if x > acc.1 {
+                acc.0.push(x);
+                acc.1 = x;
+            } else {
+                acc.0.push(acc.1);
+            }
+            acc
+        })
+        .0
+        .into_iter()
+        .rev()
+        .collect::<Vec<i32>>();
+    r.remove(0);
+    return r;
+}
 
 #[test]
 pub fn t1() {
+    assert_eq!(
+        replace_elements(vec![17, 18, 5, 4, 6, 1]),
+        vec![18, 6, 6, 6, 1, -1]
+    );
+    assert_eq!(replace_elements(vec![400]), vec![-1]);
 }
