0279d73cf7679834d267dcbe42c16428f734d60b
gengjiawen <technicalcute@gmail.com>
2022-06-18 17:42:54 +0800

    feat: add more

--- Array/_2037_minimum_number_of_moves_to_seat_everyone.rs
+++ Array/_2037_minimum_number_of_moves_to_seat_everyone.rs
@@ -1,17 +1,17 @@
 // https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone
-// 
+//
 // There are `n` seats and `n` students in a room. You are given an array `seats` of length `n`, where `seats[i]` is the position of the `i<sup>th</sup>` seat. You are also given the array `students` of length `n`, where `students[j]` is the position of the `j<sup>th</sup>` student.
-// 
+//
 // You may perform the following move any number of times:
-// 
+//
 // *   Increase or decrease the position of the `i<sup>th</sup>` student by `1` (i.e., moving the `i<sup>th</sup>` student from position `x` to `x + 1` or `x - 1`)
-// 
+//
 // Return _the **minimum number of moves** required to move each student to a seat_ _such that no two students are in the same seat._
-// 
+//
 // Note that there may be **multiple** seats or students in the **same** position at the beginning.
-// 
+//
 // **Example 1:**
-// 
+//
 // ```
 // **Input:** seats = [3,1,5], students = [2,7,4]
 // **Output:** 4
@@ -21,9 +21,9 @@
 // - The third student is moved from from position 4 to position 3 using 1 move.
 // In total, 1 + 2 + 1 = 4 moves were used.
 // ```
-// 
+//
 // **Example 2:**
-// 
+//
 // ```
 // **Input:** seats = [4,1,5,9], students = [1,3,2,6]
 // **Output:** 7
@@ -34,9 +34,9 @@
 // - The fourth student is moved from from position 6 to position 9 using 3 moves.
 // In total, 0 + 1 + 3 + 3 = 7 moves were used.
 // ```
-// 
+//
 // **Example 3:**
-// 
+//
 // ```
 // **Input:** seats = [2,2,6,6], students = [1,3,2,6]
 // **Output:** 4
@@ -48,17 +48,26 @@
 // - The fourth student is not moved.
 // In total, 1 + 3 + 0 + 0 = 4 moves were used.
 // ```
-// 
+//
 // **Constraints:**
-// 
+//
 // *   `n == seats.length == students.length`
 // *   `1 <= n <= 100`
 // *   `1 <= seats[i], students[j] <= 100`
 
 pub fn min_moves_to_seat(seats: Vec<i32>, students: Vec<i32>) -> i32 {
-
-    }
+    let mut sorted_seats = seats.clone();
+    sorted_seats.sort();
+    let mut sorted_students = students.clone();
+    sorted_students.sort();
+    return sorted_students
+        .iter()
+        .zip(sorted_seats.iter())
+        .map(|(a, b)| (a - b).abs())
+        .sum();
+}
 
 #[test]
 pub fn t1() {
+    assert_eq!(min_moves_to_seat(vec![3, 1, 5], vec![2, 7, 4]), 4);
 }
