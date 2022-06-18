422a9f8671ddc41642547e2bb5d0b545fb32f7b5
gengjiawen <technicalcute@gmail.com>
2022-06-18 17:21:41 +0800

    feat: add more

--- Array/_2089_find_target_indices_after_sorting_array.rs
+++ Array/_2089_find_target_indices_after_sorting_array.rs
@@ -1,47 +1,55 @@
 // https://leetcode.com/problems/find-target-indices-after-sorting-array
-// 
+//
 // You are given a **0-indexed** integer array `nums` and a target element `target`.
-// 
+//
 // A **target index** is an index `i` such that `nums[i] == target`.
-// 
+//
 // Return _a list of the target indices of_ `nums` after _sorting_ `nums` _in **non-decreasing** order_. If there are no target indices, return _an **empty** list_. The returned list must be sorted in **increasing** order.
-// 
+//
 // **Example 1:**
-// 
+//
 // ```
 // **Input:** nums = [1,2,5,2,3], target = 2
 // **Output:** [1,2]
 // **Explanation:** After sorting, nums is [1,<u>**2**</u>,<u>**2**</u>,3,5].
 // The indices where nums[i] == 2 are 1 and 2.
 // ```
-// 
+//
 // **Example 2:**
-// 
+//
 // ```
 // **Input:** nums = [1,2,5,2,3], target = 3
 // **Output:** [3]
 // **Explanation:** After sorting, nums is [1,2,2,<u>**3**</u>,5].
 // The index where nums[i] == 3 is 3.
 // ```
-// 
+//
 // **Example 3:**
-// 
+//
 // ```
 // **Input:** nums = [1,2,5,2,3], target = 5
 // **Output:** [4]
 // **Explanation:** After sorting, nums is [1,2,2,3,<u>**5**</u>].
 // The index where nums[i] == 5 is 4.
 // ```
-// 
+//
 // **Constraints:**
-// 
+//
 // *   `1 <= nums.length <= 100`
 // *   `1 <= nums[i], target <= 100`
 
 pub fn target_indices(nums: Vec<i32>, target: i32) -> Vec<i32> {
-
-    }
+    let mut sorted_nums = nums.clone();
+    sorted_nums.sort();
+    return sorted_nums
+        .iter()
+        .enumerate()
+        .filter(|&(_, &num)| num == target)
+        .map(|(i, _)| i as i32)
+        .collect();
+}
 
 #[test]
 pub fn t1() {
+    assert_eq!(target_indices(vec![1, 2, 5, 2, 3], 2), vec![1, 2]);
 }
--- Array/_2108_find_first_palindromic_string_in_the_array.rs
+++ Array/_2108_find_first_palindromic_string_in_the_array.rs
@@ -1,44 +1,73 @@
 // https://leetcode.com/problems/find-first-palindromic-string-in-the-array
-// 
+//
 // Given an array of strings `words`, return _the first **palindromic** string in the array_. If there is no such string, return _an **empty string**_ `""`.
-// 
+//
 // A string is **palindromic** if it reads the same forward and backward.
-// 
+//
 // **Example 1:**
-// 
+//
 // ```
 // **Input:** words = ["abc","car","ada","racecar","cool"]
 // **Output:** "ada"
 // **Explanation:** The first string that is palindromic is "ada".
 // Note that "racecar" is also palindromic, but it is not the first.
 // ```
-// 
+//
 // **Example 2:**
-// 
+//
 // ```
 // **Input:** words = ["notapalindrome","racecar"]
 // **Output:** "racecar"
 // **Explanation:** The first and only string that is palindromic is "racecar".
 // ```
-// 
+//
 // **Example 3:**
-// 
+//
 // ```
 // **Input:** words = ["def","ghi"]
 // **Output:** ""
 // **Explanation:** There are no palindromic strings, so the empty string is returned.
 // ```
-// 
+//
 // **Constraints:**
-// 
+//
 // *   `1 <= words.length <= 100`
 // *   `1 <= words[i].length <= 100`
 // *   `words[i]` consists only of lowercase English letters.
 
 pub fn first_palindrome(words: Vec<String>) -> String {
-
+    fn is_palindrome(s: &str) -> bool {
+        let mut i = 0;
+        let mut j = s.len() - 1;
+        while i < j {
+            if s.as_bytes()[i] != s.as_bytes()[j] {
+                return false;
+            }
+            i += 1;
+            j -= 1;
+        }
+        return true;
     }
+    let mut result = String::new();
+    for word in words {
+        if is_palindrome(&word) {
+            result = word;
+            break;
+        }
+    }
+    return result;
+}
 
 #[test]
 pub fn t1() {
+    assert_eq!(
+        first_palindrome(vec![
+            "abc".to_string(),
+            "car".to_string(),
+            "ada".to_string(),
+            "racecar".to_string(),
+            "cool".to_string()
+        ]),
+        "ada".to_string()
+    );
 }
