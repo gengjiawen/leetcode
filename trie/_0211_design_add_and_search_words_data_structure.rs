953d0d67ce537f46f4d4730bea01bcd100b1eca4
gengjiawen <technicalcute@gmail.com>
2022-06-22 22:22:44 +0800

    feat: add one more

--- trie/_0211_design_add_and_search_words_data_structure.rs
+++ trie/_0211_design_add_and_search_words_data_structure.rs
@@ -1,22 +1,22 @@
 // https://leetcode.com/problems/design-add-and-search-words-data-structure
-// 
+//
 // Design a data structure that supports adding new words and finding if a string matches any previously added string.
-// 
+//
 // Implement the `WordDictionary` class:
-// 
+//
 // *   `WordDictionary()` Initializes the object.
 // *   `void addWord(word)` Adds `word` to the data structure, it can be matched later.
 // *   `bool search(word)` Returns `true` if there is any string in the data structure that matches `word` or `false` otherwise. `word` may contain dots `'.'` where dots can be matched with any letter.
-// 
+//
 // **Example:**
-// 
+//
 // ```
 // **Input**
 // ["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
 // [[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
 // **Output**
 // [null,null,null,null,false,true,true,true]
-// 
+//
 // **Explanation**
 // WordDictionary wordDictionary = new WordDictionary();
 // wordDictionary.addWord("bad");
@@ -27,46 +27,68 @@
 // wordDictionary.search(".ad"); // return True
 // wordDictionary.search("b.."); // return True
 // ```
-// 
+//
 // **Constraints:**
-// 
+//
 // *   `1 <= word.length <= 25`
 // *   `word` in `addWord` consists of lowercase English letters.
 // *   `word` in `search` consist of `'.'` or lowercase English letters.
 // *   There will be at most `3` dots in `word` for `search` queries.
 // *   At most `10<sup>4</sup>` calls will be made to `addWord` and `search`.
 
+use std::collections::HashMap;
+
 struct WordDictionary {
-
+    children: HashMap<char, WordDictionary>,
+    end: bool,
 }
 
-
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
 impl WordDictionary {
-
     fn new() -> Self {
-
+        WordDictionary {
+            children: HashMap::new(),
+            end: false,
+        }
     }
-    
-    fn add_word(&self, word: String) {
 
+    fn add_word(&mut self, word: String) {
+        let mut trie = self;
+        for c in word.chars() {
+            trie = trie.children.entry(c).or_insert(WordDictionary::new());
+        }
+        trie.end = true;
     }
-    
+
     fn search(&self, word: String) -> bool {
-
+        if word.is_empty() {
+            return self.end;
+        }
+        let c = word.chars().next().unwrap();
+        if c == '.' {
+            for child in self.children.values() {
+                if Self::search(child, word[1..].to_string()) {
+                    return true;
+                }
+            }
+        } else {
+            if let Some(child) = self.children.get(&c) {
+                return Self::search(child, word[1..].to_string());
+            } else {
+                return false;
+            }
+        }
+        return false;
     }
 }
-
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * let obj = WordDictionary::new();
- * obj.add_word(word);
- * let ret_2: bool = obj.search(word);
- */
 
 #[test]
 pub fn t1() {
+    let mut trie = WordDictionary::new();
+    trie.add_word("bad".to_string());
+    trie.add_word("dad".to_string());
+    trie.add_word("mad".to_string());
+    assert_eq!(trie.search("pad".to_string()), false);
+    assert_eq!(trie.search("bad".to_string()), true);
+    assert_eq!(trie.search(".ad".to_string()), true);
+    assert_eq!(trie.search("b..".to_string()), true);
 }
--- trie/lib.rs
+++ trie/lib.rs
@@ -1,3 +1,4 @@
 #![allow(dead_code)]
 
 mod _0208_implement_trie_prefix_tree;
+mod _0211_design_add_and_search_words_data_structure;
