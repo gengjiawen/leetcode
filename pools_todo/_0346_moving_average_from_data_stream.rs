f3bcb196339c81e1f09b53648901d846fdb54da5
gengjiawen <technicalcute@gmail.com>
2022-06-26 17:21:31 +0800

    feat: prepare new todo

---
+++ pools_todo/_0346_moving_average_from_data_stream.rs
@@ -0,0 +1,1 @@
+// https://leetcode.com/problems/moving-average-from-data-stream
---
+++ pools_todo/_0961_n_repeated_element_in_size_2n_array.rs
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/n-repeated-element-in-size-2n-array
+// 
+// You are given an integer array `nums` with the following properties:
+// 
+// *   `nums.length == 2 * n`.
+// *   `nums` contains `n + 1` **unique** elements.
+// *   Exactly one element of `nums` is repeated `n` times.
+// 
+// Return _the element that is repeated_ `n` _times_.
+// 
+// **Example 1:**
+// 
+// ```
+// **Input:** nums = [1,2,3,3]
+// **Output:** 3
+// ```
+// 
+// **Example 2:**
+// 
+// ```
+// **Input:** nums = [2,1,2,5,3,2]
+// **Output:** 2
+// ```
+// 
+// **Example 3:**
+// 
+// ```
+// **Input:** nums = [5,1,5,2,5,3,5,4]
+// **Output:** 5
+// ```
+// 
+// **Constraints:**
+// 
+// *   `2 <= n <= 5000`
+// *   `nums.length == 2 * n`
+// *   `0 <= nums[i] <= 10<sup>4</sup>`
+// *   `nums` contains `n + 1` **unique** elements and one of them is repeated exactly `n` times.
+
+pub fn repeated_n_times(nums: Vec<i32>) -> i32 {
+
+    }
+
+#[test]
+pub fn t1() {
+}
