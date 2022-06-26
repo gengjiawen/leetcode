eb2f286d0d4eaf555efba82783ee7a105613cc02
gengjiawen <technicalcute@gmail.com>
2022-06-26 17:18:03 +0800

    feat: add more

--- Array/_1252_cells_with_odd_values_in_a_matrix.rs
+++ Array/_1252_cells_with_odd_values_in_a_matrix.rs
@@ -1,16 +1,16 @@
 // https://leetcode.com/problems/cells-with-odd-values-in-a-matrix
-// 
+//
 // There is an `m x n` matrix that is initialized to all `0`'s. There is also a 2D array `indices` where each `indices[i] = [r<sub>i</sub>, c<sub>i</sub>]` represents a **0-indexed location** to perform some increment operations on the matrix.
-// 
+//
 // For each location `indices[i]`, do **both** of the following:
-// 
+//
 // 1.  Increment **all** the cells on row `r<sub>i</sub>`.
 // 2.  Increment **all** the cells on column `c<sub>i</sub>`.
-// 
+//
 // Given `m`, `n`, and `indices`, return _the **number of odd-valued cells** in the matrix after applying the increment to all locations in_ `indices`.
-// 
+//
 // **Example 1:**
-// 
+//
 // ![](https://assets.leetcode.com/uploads/2019/10/30/e1.png)
 // ```
 // **Input:** m = 2, n = 3, indices = [[0,1],[1,1]]
@@ -19,29 +19,50 @@
 // After applying first increment it becomes [[1,2,1],[0,1,0]].
 // The final matrix is [[1,3,1],[1,3,1]], which contains 6 odd numbers.
 // ```
-// 
+//
 // **Example 2:**
-// 
+//
 // ![](https://assets.leetcode.com/uploads/2019/10/30/e2.png)
 // ```
 // **Input:** m = 2, n = 2, indices = [[1,1],[0,0]]
 // **Output:** 0
 // **Explanation:** Final matrix = [[2,2],[2,2]]. There are no odd numbers in the final matrix.
 // ```
-// 
+//
 // **Constraints:**
-// 
+//
 // *   `1 <= m, n <= 50`
 // *   `1 <= indices.length <= 100`
 // *   `0 <= r<sub>i</sub> < m`
 // *   `0 <= c<sub>i</sub> < n`
-// 
+//
 // **Follow up:** Could you solve this in `O(n + m + indices.length)` time with only `O(n + m)` extra space?
 
 pub fn odd_cells(m: i32, n: i32, indices: Vec<Vec<i32>>) -> i32 {
-
+    let mut matrix = vec![vec![0; n as usize]; m as usize];
+    for index in indices {
+        let row = index[0];
+        let col = index[1];
+        for i in 0..n {
+            matrix[row as usize][i as usize] += 1;
+        }
+        for i in 0..m {
+            matrix[i as usize][col as usize] += 1;
+        }
     }
+
+    let mut count = 0;
+    for row in matrix {
+        for cell in row {
+            if cell % 2 == 1 {
+                count += 1;
+            }
+        }
+    }
+    return count;
+}
 
 #[test]
 pub fn t1() {
+    assert_eq!(odd_cells(2, 3, vec![vec![0, 1], vec![1, 1]]), 6);
 }
--- Array/_1351_count_negative_numbers_in_a_sorted_matrix.rs
+++ Array/_1351_count_negative_numbers_in_a_sorted_matrix.rs
@@ -1,35 +1,53 @@
 // https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix
-// 
+//
 // Given a `m x n` matrix `grid` which is sorted in non-increasing order both row-wise and column-wise, return _the number of **negative** numbers in_ `grid`.
-// 
+//
 // **Example 1:**
-// 
+//
 // ```
 // **Input:** grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
 // **Output:** 8
 // **Explanation:** There are 8 negatives number in the matrix.
 // ```
-// 
+//
 // **Example 2:**
-// 
+//
 // ```
 // **Input:** grid = [[3,2],[1,0]]
 // **Output:** 0
 // ```
-// 
+//
 // **Constraints:**
-// 
+//
 // *   `m == grid.length`
 // *   `n == grid[i].length`
 // *   `1 <= m, n <= 100`
 // *   `-100 <= grid[i][j] <= 100`
-// 
+//
 // **Follow up:** Could you find an `O(n + m)` solution?
 
 pub fn count_negatives(grid: Vec<Vec<i32>>) -> i32 {
-
+    let mut count = 0;
+    for row in grid {
+        for (index, cell) in row.iter().enumerate() {
+            if *cell < 0 {
+                count = count + row.len() as i32 - index as i32;
+                break;
+            }
+        }
     }
+    return count;
+}
 
 #[test]
 pub fn t1() {
+    assert_eq!(
+        count_negatives(vec![
+            vec![4, 3, 2, -1],
+            vec![3, 2, 1, -1],
+            vec![1, 1, -1, -2],
+            vec![-1, -1, -2, -3]
+        ]),
+        8
+    );
 }
