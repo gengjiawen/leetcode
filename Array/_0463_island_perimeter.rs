45e52e441fba047ac9d9abf72b9753384f46e2ef
gengjiawen <technicalcute@gmail.com>
2022-06-28 20:05:56 +0800

    feat: add more

--- Array/_0463_island_perimeter.rs
+++ Array/_0463_island_perimeter.rs
@@ -1,36 +1,36 @@
 // https://leetcode.com/problems/island-perimeter
-// 
+//
 // You are given `row x col` `grid` representing a map where `grid[i][j] = 1` represents land and `grid[i][j] = 0` represents water.
-// 
+//
 // Grid cells are connected **horizontally/vertically** (not diagonally). The `grid` is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
-// 
+//
 // The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1\. The grid is rectangular, width and height don't exceed 100\. Determine the perimeter of the island.
-// 
+//
 // **Example 1:**
-// 
+//
 // ![](https://assets.leetcode.com/uploads/2018/10/12/island.png)
 // ```
 // **Input:** grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
 // **Output:** 16
 // **Explanation:** The perimeter is the 16 yellow stripes in the image above.
 // ```
-// 
+//
 // **Example 2:**
-// 
+//
 // ```
 // **Input:** grid = [[1]]
 // **Output:** 4
 // ```
-// 
+//
 // **Example 3:**
-// 
+//
 // ```
 // **Input:** grid = [[1,0]]
 // **Output:** 4
 // ```
-// 
+//
 // **Constraints:**
-// 
+//
 // *   `row == grid.length`
 // *   `col == grid[i].length`
 // *   `1 <= row, col <= 100`
@@ -38,9 +38,32 @@
 // *   There is exactly one island in `grid`.
 
 pub fn island_perimeter(grid: Vec<Vec<i32>>) -> i32 {
-
+    let mut perimeter = 0;
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            if grid[i][j] == 1 {
+                perimeter += 4;
+                if i > 0 && grid[i - 1][j] == 1 {
+                    perimeter -= 2;
+                }
+                if j > 0 && grid[i][j - 1] == 1 {
+                    perimeter -= 2;
+                }
+            }
+        }
     }
+    return perimeter;
+}
 
 #[test]
 pub fn t1() {
+    assert_eq!(
+        island_perimeter(vec![
+            vec![0, 1, 0, 0],
+            vec![1, 1, 1, 0],
+            vec![0, 1, 0, 0],
+            vec![1, 1, 0, 0]
+        ]),
+        16
+    );
 }
--- Array/_0766_toeplitz_matrix.rs
+++ Array/_0766_toeplitz_matrix.rs
@@ -1,11 +1,11 @@
 // https://leetcode.com/problems/toeplitz-matrix
-// 
+//
 // Given an `m x n` `matrix`, return _`true` if the matrix is Toeplitz. Otherwise, return `false`._
-// 
+//
 // A matrix is **Toeplitz** if every diagonal from top-left to bottom-right has the same elements.
-// 
+//
 // **Example 1:**
-// 
+//
 // ![](https://assets.leetcode.com/uploads/2020/11/04/ex1.jpg)
 // ```
 // **Input:** matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
@@ -15,9 +15,9 @@
 // "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
 // In each diagonal all elements are the same, so the answer is True.
 // ```
-// 
+//
 // **Example 2:**
-// 
+//
 // ![](https://assets.leetcode.com/uploads/2020/11/04/ex2.jpg)
 // ```
 // **Input:** matrix = [[1,2],[2,2]]
@@ -25,23 +25,34 @@
 // **Explanation:**
 // The diagonal "[1, 2]" has different elements.
 // ```
-// 
+//
 // **Constraints:**
-// 
+//
 // *   `m == matrix.length`
 // *   `n == matrix[i].length`
 // *   `1 <= m, n <= 20`
 // *   `0 <= matrix[i][j] <= 99`
-// 
+//
 // **Follow up:**
-// 
+//
 // *   What if the `matrix` is stored on disk, and the memory is limited such that you can only load at most one row of the matrix into the memory at once?
 // *   What if the `matrix` is so large that you can only load up a partial row into the memory at once?
 
 pub fn is_toeplitz_matrix(matrix: Vec<Vec<i32>>) -> bool {
-
+    for i in 0..matrix.len() {
+        for j in 0..matrix[0].len() {
+            if i > 0 && j > 0 && matrix[i][j] != matrix[i - 1][j - 1] {
+                return false;
+            }
+        }
     }
+    return true;
+}
 
 #[test]
 pub fn t1() {
+    assert_eq!(
+        is_toeplitz_matrix(vec![vec![1, 2, 3, 4], vec![5, 1, 2, 3], vec![9, 5, 1, 2]]),
+        true
+    );
 }
