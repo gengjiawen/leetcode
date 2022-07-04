bb443fa22935b78769265166fae0567179a3390b
gengjiawen <technicalcute@gmail.com>
2022-07-04 22:40:50 +0800

    feat: add more

--- greedy/_1217_minimum_cost_to_move_chips_to_the_same_position.rs
+++ greedy/_1217_minimum_cost_to_move_chips_to_the_same_position.rs
@@ -1,16 +1,16 @@
 // https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position
-// 
+//
 // We have `n` chips, where the position of the `i<sup>th</sup>` chip is `position[i]`.
-// 
+//
 // We need to move all the chips to **the same position**. In one step, we can change the position of the `i<sup>th</sup>` chip from `position[i]` to:
-// 
+//
 // *   `position[i] + 2` or `position[i] - 2` with `cost = 0`.
 // *   `position[i] + 1` or `position[i] - 1` with `cost = 1`.
-// 
+//
 // Return _the minimum cost_ needed to move all the chips to the same position.
-// 
+//
 // **Example 1:**
-// 
+//
 // ![](https://assets.leetcode.com/uploads/2020/08/15/chips_e1.jpg)
 // ```
 // **Input:** position = [1,2,3]
@@ -19,32 +19,42 @@
 // Second step: Move the chip at position 2 to position 1 with cost = 1.
 // Total cost is 1.
 // ```
-// 
+//
 // **Example 2:**
-// 
+//
 // ![](https://assets.leetcode.com/uploads/2020/08/15/chip_e2.jpg)
 // ```
 // **Input:** position = [2,2,2,3,3]
 // **Output:** 2
 // **Explanation:** We can move the two chips at position  3 to position 2\. Each move has cost = 1\. The total cost = 2.
 // ```
-// 
+//
 // **Example 3:**
-// 
+//
 // ```
 // **Input:** position = [1,1000000000]
 // **Output:** 1
 // ```
-// 
+//
 // **Constraints:**
-// 
+//
 // *   `1 <= position.length <= 100`
 // *   `1 <= position[i] <= 10^9`
 
 pub fn min_cost_to_move_chips(position: Vec<i32>) -> i32 {
-
+    let mut even = 0;
+    let mut odd = 0;
+    for &p in &position {
+        if p % 2 == 0 {
+            even += 1;
+        } else {
+            odd += 1;
+        }
     }
+    return even.min(odd);
+}
 
 #[test]
 pub fn t1() {
+    assert_eq!(min_cost_to_move_chips(vec![1, 2, 3]), 1);
 }
--- greedy/lib.rs
+++ greedy/lib.rs
@@ -2,4 +2,5 @@
 
 mod _0812_largest_triangle_area;
 mod _0942_di_string_match;
+mod _1217_minimum_cost_to_move_chips_to_the_same_position;
 mod _1403_minimum_subsequence_in_non_increasing_order;
