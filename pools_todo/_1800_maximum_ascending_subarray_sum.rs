0ba86989e15379fbcae454c897ff9d577db11cab
gengjiawen <technicalcute@gmail.com>
2022-07-04 22:50:48 +0800

    feat: prepare new todo

---
+++ pools_todo/_1800_maximum_ascending_subarray_sum.rs
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/maximum-ascending-subarray-sum
+// 
+// Given an array of positive integers `nums`, return the _maximum possible sum of an **ascending** subarray in_ `nums`.
+// 
+// A subarray is defined as a contiguous sequence of numbers in an array.
+// 
+// A subarray `[nums<sub>l</sub>, nums<sub>l+1</sub>, ..., nums<sub>r-1</sub>, nums<sub>r</sub>]` is **ascending** if for all `i` where `l <= i < r`, `nums<sub>i</sub> < nums<sub>i+1</sub>`. Note that a subarray of size `1` is **ascending**.
+// 
+// **Example 1:**
+// 
+// ```
+// **Input:** nums = [10,20,30,5,10,50]
+// **Output:** 65
+// **Explanation:** [5,10,50] is the ascending subarray with the maximum sum of 65.
+// ```
+// 
+// **Example 2:**
+// 
+// ```
+// **Input:** nums = [10,20,30,40,50]
+// **Output:** 150
+// **Explanation:** [10,20,30,40,50] is the ascending subarray with the maximum sum of 150.
+// ```
+// 
+// **Example 3:**
+// 
+// ```
+// **Input:** nums = [12,17,15,13,10,11,12]
+// **Output:** 33
+// **Explanation:** [10,11,12] is the ascending subarray with the maximum sum of 33.
+// ```
+// 
+// **Constraints:**
+// 
+// *   `1 <= nums.length <= 100`
+// *   `1 <= nums[i] <= 100`
+
+pub fn max_ascending_sum(nums: Vec<i32>) -> i32 {
+
+    }
+
+#[test]
+pub fn t1() {
+}
---
+++ pools_todo/_2248_intersection_of_multiple_arrays.rs
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/intersection-of-multiple-arrays
+// 
+// Given a 2D integer array `nums` where `nums[i]` is a non-empty array of **distinct** positive integers, return _the list of integers that are present in **each array** of_ `nums` _sorted in **ascending order**_.
+// 
+// **Example 1:**
+// 
+// ```
+// **Input:** nums = [[<u>**3**</u>,1,2,<u>**4**</u>,5],[1,2,<u>**3**</u>,<u>**4**</u>],[<u>**3**</u>,<u>**4**</u>,5,6]]
+// **Output:** [3,4]
+// **Explanation:** 
+// The only integers present in each of nums[0] = [<u>**3**</u>,1,2,<u>**4**</u>,5], nums[1] = [1,2,<u>**3**</u>,<u>**4**</u>], and nums[2] = [<u>**3**</u>,<u>**4**</u>,5,6] are 3 and 4, so we return [3,4].```
+// 
+// **Example 2:**
+// 
+// ```
+// **Input:** nums = [[1,2,3],[4,5,6]]
+// **Output:** []
+// **Explanation:** 
+// There does not exist any integer present both in nums[0] and nums[1], so we return an empty list [].
+// ```
+// 
+// **Constraints:**
+// 
+// *   `1 <= nums.length <= 1000`
+// *   `1 <= sum(nums[i].length) <= 1000`
+// *   `1 <= nums[i][j] <= 1000`
+// *   All the values of `nums[i]` are **unique**.
+
+pub fn intersection(nums: Vec<Vec<i32>>) -> Vec<i32> {
+
+    }
+
+#[test]
+pub fn t1() {
+}
