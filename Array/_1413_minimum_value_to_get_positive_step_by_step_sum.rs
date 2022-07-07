c99d235d76ddb5bf3bc311033840563fd9f9ce7f
gengjiawen <technicalcute@gmail.com>
2022-07-07 22:44:40 +0800

    feat: add more

--- Array/_1413_minimum_value_to_get_positive_step_by_step_sum.rs
+++ Array/_1413_minimum_value_to_get_positive_step_by_step_sum.rs
@@ -1,13 +1,13 @@
 // https://leetcode.com/problems/minimum-value-to-get-positive-step-by-step-sum
-// 
+//
 // Given an array of integers `nums`, you start with an initial **positive** value _startValue__._
-// 
+//
 // In each iteration, you calculate the step by step sum of _startValue_ plus elements in `nums` (from left to right).
-// 
+//
 // Return the minimum **positive** value of _startValue_ such that the step by step sum is never less than 1.
-// 
+//
 // **Example 1:**
-// 
+//
 // ```
 // **Input:** nums = [-3,2,-3,4,2]
 // **Output:** 5
@@ -20,31 +20,38 @@
 //   (0 **+4** ) = 4  | (1 **+4** ) = 5    |   4
 //   (4 **+2** ) = 6  | (5 **+2** ) = 7    |   2
 // ```
-// 
+//
 // **Example 2:**
-// 
+//
 // ```
 // **Input:** nums = [1,2]
 // **Output:** 1
-// **Explanation:** Minimum start value should be positive. 
+// **Explanation:** Minimum start value should be positive.
 // ```
-// 
+//
 // **Example 3:**
-// 
+//
 // ```
 // **Input:** nums = [1,-2,-3]
 // **Output:** 5
 // ```
-// 
+//
 // **Constraints:**
-// 
+//
 // *   `1 <= nums.length <= 100`
 // *   `-100 <= nums[i] <= 100`
 
 pub fn min_start_value(nums: Vec<i32>) -> i32 {
-
+    let mut min_value = 0;
+    let mut cur = 0;
+    for num in nums {
+        cur += num;
+        min_value = min_value.min(cur);
     }
+    return std::cmp::max(-min_value + 1, 1);
+}
 
 #[test]
 pub fn t1() {
+    assert_eq!(min_start_value(vec![-3, 2, -3, 4, 2]), 5);
 }
--- Array/_1502_can_make_arithmetic_progression_from_sequence.rs
+++ Array/_1502_can_make_arithmetic_progression_from_sequence.rs
@@ -1,34 +1,43 @@
 // https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence
-// 
+//
 // A sequence of numbers is called an **arithmetic progression** if the difference between any two consecutive elements is the same.
-// 
+//
 // Given an array of numbers `arr`, return `true` _if the array can be rearranged to form an **arithmetic progression**. Otherwise, return_ `false`.
-// 
+//
 // **Example 1:**
-// 
+//
 // ```
 // **Input:** arr = [3,5,1]
 // **Output:** true
 // **Explanation:** We can reorder the elements as [1,3,5] or [5,3,1] with differences 2 and -2 respectively, between each consecutive elements.
 // ```
-// 
+//
 // **Example 2:**
-// 
+//
 // ```
 // **Input:** arr = [1,2,4]
 // **Output:** false
 // **Explanation:** There is no way to reorder the elements to obtain an arithmetic progression.
 // ```
-// 
+//
 // **Constraints:**
-// 
+//
 // *   `2 <= arr.length <= 1000`
 // *   `-10<sup>6</sup> <= arr[i] <= 10<sup>6</sup>`
 
 pub fn can_make_arithmetic_progression(arr: Vec<i32>) -> bool {
-
+    let mut sorted_arr = arr;
+    sorted_arr.sort_unstable();
+    let diff = sorted_arr[1] - sorted_arr[0];
+    for i in 2..sorted_arr.len() {
+        if sorted_arr[i] - sorted_arr[i - 1] != diff {
+            return false;
+        }
     }
+    return true;
+}
 
 #[test]
 pub fn t1() {
+    assert_eq!(can_make_arithmetic_progression(vec![3, 5, 1]), true);
 }
