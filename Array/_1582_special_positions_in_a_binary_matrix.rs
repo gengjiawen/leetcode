269542d0c4c05e132793a64c806cf187ecc0e57c
gengjiawen <technicalcute@gmail.com>
2022-07-08 23:04:15 +0800

    feat: add more

--- Array/_1582_special_positions_in_a_binary_matrix.rs
+++ Array/_1582_special_positions_in_a_binary_matrix.rs
@@ -1,38 +1,69 @@
 // https://leetcode.com/problems/special-positions-in-a-binary-matrix
-// 
+//
 // Given an `m x n` binary matrix `mat`, return _the number of special positions in_ `mat`_._
-// 
+//
 // A position `(i, j)` is called **special** if `mat[i][j] == 1` and all other elements in row `i` and column `j` are `0` (rows and columns are **0-indexed**).
-// 
+//
 // **Example 1:**
-// 
+//
 // ![](https://assets.leetcode.com/uploads/2021/12/23/special1.jpg)
 // ```
 // **Input:** mat = [[1,0,0],[0,0,1],[1,0,0]]
 // **Output:** 1
 // **Explanation:** (1, 2) is a special position because mat[1][2] == 1 and all other elements in row 1 and column 2 are 0.
 // ```
-// 
+//
 // **Example 2:**
-// 
+//
 // ![](https://assets.leetcode.com/uploads/2021/12/24/special-grid.jpg)
 // ```
 // **Input:** mat = [[1,0,0],[0,1,0],[0,0,1]]
 // **Output:** 3
 // **Explanation:** (0, 0), (1, 1) and (2, 2) are special positions.
 // ```
-// 
+//
 // **Constraints:**
-// 
+//
 // *   `m == mat.length`
 // *   `n == mat[i].length`
 // *   `1 <= m, n <= 100`
 // *   `mat[i][j]` is either `0` or `1`.
 
 pub fn num_special(mat: Vec<Vec<i32>>) -> i32 {
-
+    fn is_special(mat: &Vec<Vec<i32>>, p1: usize, p2: usize) -> bool {
+        for i in 0..mat.len() {
+            if i == p1 {
+                continue;
+            }
+            if mat[i][p2] == 1 {
+                return false;
+            }
+        }
+        for j in 0..mat[0].len() {
+            if j == p2 {
+                continue;
+            }
+            if mat[p1][j] == 1 {
+                return false;
+            }
+        }
+        return true;
     }
+    let mut count = 0;
+    for i in 0..mat.len() {
+        for j in 0..mat[0].len() {
+            if mat[i][j] == 1 && is_special(&mat, i, j) {
+                count += 1;
+            }
+        }
+    }
+    return count;
+}
 
 #[test]
 pub fn t1() {
+    assert_eq!(
+        num_special(vec![vec![1, 0, 0], vec![0, 0, 1], vec![1, 0, 0]]),
+        1
+    );
 }
--- Array/_1619_mean_of_array_after_removing_some_elements.rs
+++ Array/_1619_mean_of_array_after_removing_some_elements.rs
@@ -1,41 +1,54 @@
 // https://leetcode.com/problems/mean-of-array-after-removing-some-elements
-// 
+//
 // Given an integer array `arr`, return _the mean of the remaining integers after removing the smallest `5%` and the largest `5%` of the elements._
-// 
+//
 // Answers within `10<sup>-5</sup>` of the **actual answer** will be considered accepted.
-// 
+//
 // **Example 1:**
-// 
+//
 // ```
 // **Input:** arr = [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3]
 // **Output:** 2.00000
 // **Explanation:** After erasing the minimum and the maximum values of this array, all elements are equal to 2, so the mean is 2.
 // ```
-// 
+//
 // **Example 2:**
-// 
+//
 // ```
 // **Input:** arr = [6,2,7,5,1,2,0,3,10,2,5,0,5,5,0,8,7,6,8,0]
 // **Output:** 4.00000
 // ```
-// 
+//
 // **Example 3:**
-// 
+//
 // ```
 // **Input:** arr = [6,0,7,0,7,5,7,8,3,4,0,7,8,1,6,8,1,1,2,4,8,1,9,5,4,3,8,5,10,8,6,6,1,0,6,10,8,2,3,4]
 // **Output:** 4.77778
 // ```
-// 
+//
 // **Constraints:**
-// 
+//
 // *   `20 <= arr.length <= 1000`
 // *   `arr.length`**is a multiple** of `20`.
 // *   `<font face="monospace">0 <= arr[i] <= 10<sup>5</sup></font>`
 
 pub fn trim_mean(arr: Vec<i32>) -> f64 {
-
+    let mut sorted_arr = arr;
+    sorted_arr.sort_unstable();
+    let drop_count = (sorted_arr.len() as f64 * 0.05) as usize;
+    let mut sum = 0;
+    for i in drop_count..sorted_arr.len() - drop_count {
+        sum += sorted_arr[i];
     }
+    return sum as f64 / (sorted_arr.len() - 2 * drop_count) as f64;
+}
 
 #[test]
 pub fn t1() {
+    assert_eq!(
+        trim_mean(vec![
+            1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3
+        ]),
+        2.0
+    );
 }
