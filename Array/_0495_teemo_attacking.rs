acf2d287266684aec2d50b131ded281a06df33fa
gengjiawen <technicalcute@gmail.com>
2022-08-01 23:14:43 +0800

    feat: add more

--- Array/_0495_teemo_attacking.rs
+++ Array/_0495_teemo_attacking.rs
@@ -1,13 +1,13 @@
 // https://leetcode.com/problems/teemo-attacking
-// 
+//
 // Our hero Teemo is attacking an enemy Ashe with poison attacks! When Teemo attacks Ashe, Ashe gets poisoned for a exactly `duration` seconds. More formally, an attack at second `t` will mean Ashe is poisoned during the **inclusive** time interval `[t, t + duration - 1]`. If Teemo attacks again **before** the poison effect ends, the timer for it is **reset**, and the poison effect will end `duration` seconds after the new attack.
-// 
+//
 // You are given a **non-decreasing** integer array `timeSeries`, where `timeSeries[i]` denotes that Teemo attacks Ashe at second `timeSeries[i]`, and an integer `duration`.
-// 
+//
 // Return _the **total** number of seconds that Ashe is poisoned_.
-// 
+//
 // **Example 1:**
-// 
+//
 // ```
 // **Input:** timeSeries = [1,4], duration = 2
 // **Output:** 4
@@ -16,9 +16,9 @@
 // - At second 4, Teemo attacks, and Ashe is poisoned for seconds 4 and 5.
 // Ashe is poisoned for seconds 1, 2, 4, and 5, which is 4 seconds in total.
 // ```
-// 
+//
 // **Example 2:**
-// 
+//
 // ```
 // **Input:** timeSeries = [1,2], duration = 2
 // **Output:** 3
@@ -26,17 +26,28 @@
 // - At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
 // - At second 2 however, Teemo attacks again and resets the poison timer. Ashe is poisoned for seconds 2 and 3.
 // Ashe is poisoned for seconds 1, 2, and 3, which is 3 seconds in total.```
-// 
+//
 // **Constraints:**
-// 
+//
 // *   `1 <= timeSeries.length <= 10<sup>4</sup>`
 // *   `0 <= timeSeries[i], duration <= 10<sup>7</sup>`
 // *   `timeSeries` is sorted in **non-decreasing** order.
 
 pub fn find_poisoned_duration(time_series: Vec<i32>, duration: i32) -> i32 {
-
+    let mut poisoned_duration = duration;
+    for i in 1..time_series.len() {
+        let start = time_series[i - 1];
+        let end = time_series[i];
+        if start + duration <= end {
+            poisoned_duration += duration;
+        } else {
+            poisoned_duration += end - start;
+        }
     }
+    return poisoned_duration;
+}
 
 #[test]
 pub fn t1() {
+    assert_eq!(find_poisoned_duration(vec![1, 4], 2), 4);
 }
