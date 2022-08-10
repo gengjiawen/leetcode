276cdfc3b03e3099ed043a74d6e981c28ccc5b48
Jiawen Geng <technicalcute@gmail.com>
2022-08-10 05:35:08 +0000

    feat: add more

--- pools_todo/_1064_fixed_point.rs
+++ pools_todo/_1064_fixed_point.rs
@@ -1,1 +1,15 @@
 // https://leetcode.com/problems/fixed-point
+
+pub fn fixed_point(arr: Vec<i32>) -> i32 {
+    for i in 0..arr.len() {
+        if arr[i] == i as i32 {
+            return i as i32;
+        }
+    }
+    return -1;
+}
+
+#[test]
+pub fn t1() {
+    assert_eq!(fixed_point(vec![-10, -5, 0, 3, 7]), 3);
+}
--- pools_todo/_1065_index_pairs_of_a_string.rs
+++ pools_todo/_1065_index_pairs_of_a_string.rs
@@ -1,1 +1,41 @@
 // https://leetcode.com/problems/index-pairs-of-a-string
+
+pub fn index_pairs(text: String, words: Vec<String>) -> Vec<Vec<i32>> {
+    let mut result = Vec::new();
+    let text_len = text.len();
+    let hs: std::collections::HashSet<String> = std::collections::HashSet::from_iter(words);
+    for i in 0..text_len {
+        for j in i..text_len {
+            if hs.contains(&text[i..=j].to_string()) {
+                result.push(vec![i as i32, j as i32]);
+            }
+        }
+    }
+    return result;
+}
+
+#[test]
+pub fn t1() {
+    assert_eq!(
+        index_pairs(
+            "thestoryofleetcodeandme".to_string(),
+            vec![
+                "story".to_string(),
+                "fleet".to_string(),
+                "leetcode".to_string(),
+            ]
+        ),
+        vec![vec![3, 7], vec![9, 13], vec![10, 17]]
+    );
+    assert_eq!(
+        index_pairs(
+            "ababa".to_string(),
+            vec![
+                "aba".to_string(),
+                "ab".to_string(),
+            ]
+        ),
+        vec![vec![0, 1], vec![0, 2], vec![2, 3], vec![2, 4]]
+    );
+}
+
