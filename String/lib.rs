c2a941d5535535f37348051e02931ec60103deec
gengjiawen <technicalcute@gmail.com>
2022-08-20 21:59:18 +0800

    feat: add more

--- String/lib.rs
+++ String/lib.rs
@@ -5,3 +5,4 @@ mod _1119_remove_vowels_from_a_string;
 mod _1859_sorting_the_sentence;
 mod _2042_check_if_numbers_are_ascending_in_a_sentence;
 mod _2325_decode_the_message;
+mod _1678_goal_parser_interpretation;
